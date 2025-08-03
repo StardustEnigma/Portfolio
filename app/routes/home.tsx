90d2fa5b47139da746271fbf389a955b836087ae
Atharva Mandle <atharvamandle19@gmail.com>
2025-08-03 11:40:32 +0530

    background added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,6 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import DotGrid from '../welcome/welcome';
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +8,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="h-screen w-screen">
+      <DotGrid />
+    </div>
+  );
 }
